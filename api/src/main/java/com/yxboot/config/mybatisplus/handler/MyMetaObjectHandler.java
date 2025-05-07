3c95b0a372d402ff3930b05d27f987e1f00f7ee0
boyazuo <6989006@qq.com>
2025-05-07 22:35:41 +0800

    fix: 修改 MyBatis Plus 自动注入

--- api/src/main/java/com/yxboot/config/mybatisplus/handler/MyMetaObjectHandler.java
+++ api/src/main/java/com/yxboot/config/mybatisplus/handler/MyMetaObjectHandler.java
@@ -1,11 +1,16 @@
 package com.yxboot.config.mybatisplus.handler;
 
+import java.time.LocalDateTime;
+
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.yxboot.config.security.SecurityUser;
 
-import cn.hutool.core.date.LocalDateTimeUtil;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -18,13 +23,25 @@ import lombok.extern.slf4j.Slf4j;
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-        if (getFieldValByName("createTime", metaObject) == null) {
-            this.setFieldValByName("createTime", LocalDateTimeUtil.now(), metaObject);
-        }
+        this.strictInsertFill(metaObject, "create_user_id", Long.class, getCurrentUserId());
+        this.strictInsertFill(metaObject, "create_time", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "update_user_id", Long.class, getCurrentUserId());
+        this.strictInsertFill(metaObject, "update_time", LocalDateTime.class, LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("updateTime", LocalDateTimeUtil.now(), metaObject);
+        this.strictUpdateFill(metaObject, "update_user_id", Long.class, getCurrentUserId());
+        this.strictUpdateFill(metaObject, "update_time", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    private Long getCurrentUserId() {
+        // 获取当前用户id
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && authentication.getName() != null
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            return ((SecurityUser) authentication.getPrincipal()).getUserId();
+        }
+        return null;
     }
 }
